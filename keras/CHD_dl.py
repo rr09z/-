d19af9d61b2084170a68512c0856d057e2b3a09c
rr09z <69394139+rr09z@users.noreply.github.com>
2020-10-25 02:15:44 -0400

    Add files via upload

--- keras/CHD_dl.py
+++ keras/CHD_dl.py
@@ -16,7 +16,21 @@ model.add(Dense(8, activation='relu'))
 model.add(Dense(1, activation='sigmoid'))
 
 model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+result = []
+epochs_test = [150, 300, 600, 800, 1000]
+for i in epochs_test:
+    model.fit(X, y, epochs=i, batch_size=10)
+    _, accuracy = model.evaluate(X, y)
+    result.append(accuracy)
+    print('Accuracy: %.2f' % (accuracy*100))#可以说屌用没有，输入一万次怕是都这个吊样
 
-model.fit(X, y, epochs=150, batch_size=10)
-_, accuracy = model.evaluate(X, y)
-print('Accuracy: %.2f' % (accuracy*100))
+time = [1, 2, 3, 4, 5]
+result2 = []
+for i in time:
+    if i < 5:
+        model.fit(X, y, epochs=150, batch_size=10)
+        _, accuracy = model.evaluate(X, y)
+        result2.append(accuracy)
+        print('Accuracy: %.2f' % (accuracy * 100))
+    else:
+        break
